632aef91c2fd499ca6da49e89cc2ae3f533a0ad9
Dinh Xuan <xuanyeumy123@gmail.com>
2025-04-04 21:20:20 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenDoVietHoang_1911061220.Models
+{
+   public class ErrorViewModel
+   {
+      public string? RequestId { get; set; }
+
+      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+   }
+}
